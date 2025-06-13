e0d5883c90ed5959ec23ef0acb73ad1479849853
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-06-13 11:45:41 +0200

    dummy basic-package.ejs

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/basic-package", (req, res) => {
+  res.render("basic-package.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
