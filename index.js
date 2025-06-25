1358a114b0214f455d2b6728174b96be084832f4
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-06-25 18:22:20 +0200

    npm i winston

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use((req, res, next) => {
-  const visit = {
-    timestamp: new Date().toISOString(),
-    ip: req.ip,
-    url: req.originalUrl,
-    userAgent: req.get("User-Agent"),
-    referrer: req.get("Referer") || "Direct",
-  };
-  console.log(visit); // or save to DB
-  next();
-});
+// app.use((req, res, next) => {
+//   const visit = {
+//     timestamp: new Date().toISOString(),
+//     ip: req.ip,
+//     url: req.originalUrl,
+//     userAgent: req.get("User-Agent"),
+//     referrer: req.get("Referer") || "Direct",
+//   };
+//   console.log(visit); // or save to DB
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.render("./index.ejs");
