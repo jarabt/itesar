b12b3ebc2298bed98ab645eb1e5294c5b4436fa3
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-06-30 10:37:07 +0200

    working navigation icon to dummy standard-package.ejs

--- index.js
+++ index.js
@@ -52,6 +52,18 @@ app.get("/basic-package", (req, res) => {
   res.render("basic-package.ejs");
 });
 
+app.get("/standard-package", (req, res) => {
+  // Log the visitor
+  const visit = {
+    url: req.originalUrl,
+    ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    referrer: req.get("Referer") || "Direct",
+    userAgent: req.get("User-Agent"),
+  };
+  logger.info(visit);
+  res.render("standard-package.ejs");
+});
+
 app.listen(port, () => {
   //console.log(`Server running on port ${port}`);
   logger.info(`Server running on port ${port}`);
