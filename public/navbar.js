8a7b52591ab4ef5adab6b535945ba8e0d6d39cbc
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-07-31 15:33:26 +0200

    copilot humburger responsivness menu

---
+++ public/navbar.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+toggle.addEventListener("click", function () {
+  menu.classList.toggle("open");
+});
