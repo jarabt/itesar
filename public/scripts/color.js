b5ded25749cca73b9e45b23f3024663c15feadd3
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-10-17 18:19:21 +0200

    splitted rgb destructed

--- public/scripts/color.js
+++ public/scripts/color.js
@@ -1,5 +1,15 @@
 const colorInput = document.getElementById("color");
+const redSpan = document.querySelector(".red");
+const greenSpan = document.querySelector(".green");
+const blueSpan = document.querySelector(".blue");
+
 colorInput.addEventListener("input", () => {
   const value = colorInput.value;
   const arrayFromSplitted = value.split(",");
+  if (arrayFromSplitted.length === 3) {
+    const [r, g, b] = arrayFromSplitted;
+    redSpan.innerHTML = r;
+    greenSpan.innerHTML = g;
+    blueSpan.innerHTML = b;
+  }
 });
