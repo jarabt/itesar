b31ba41186a94336a0ad548f96028ee9d54b974d
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-10-17 18:50:10 +0200

    color input replace non digit

--- public/scripts/color.js
+++ public/scripts/color.js
@@ -4,12 +4,18 @@ const greenSpan = document.querySelector(".green");
 const blueSpan = document.querySelector(".blue");
 
 colorInput.addEventListener("input", () => {
-  const value = colorInput.value;
-  const arrayFromSplitted = value.split(",");
-  if (arrayFromSplitted.length === 3) {
-    const [r, g, b] = arrayFromSplitted;
-    redSpan.innerHTML = r;
-    greenSpan.innerHTML = g;
-    blueSpan.innerHTML = b;
-  }
+  const value = colorInput.value.replace(/\D/g, " ");
+  redSpan.innerHTML = value;
+  //   const arrayFromSplitted = value.split(",");
+  //   arrayFromSplitted.map();
+  //   if (arrayFromSplitted.length === 3) {
+  //     alert("ok");
+  //     const [r, g, b] = arrayFromSplitted;
+  //     redSpan.innerHTML = r;
+  //     greenSpan.innerHTML = g;
+  //     blueSpan.innerHTML = b;
+  //   } else {
+  //     redSpan.innerHTML = "not 3";
+  //     alert("ok");
+  //   }
 });
