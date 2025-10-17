ed7e1a3c9ec018275178b9ea67e602dd8d85a263
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-10-17 19:25:10 +0200

    rgb code if separated by comma

--- public/scripts/color.js
+++ public/scripts/color.js
@@ -3,19 +3,25 @@ const redSpan = document.querySelector(".red");
 const greenSpan = document.querySelector(".green");
 const blueSpan = document.querySelector(".blue");
 
+const testSpan = document.querySelector(".test");
+
 colorInput.addEventListener("input", () => {
-  const value = colorInput.value.replace(/\D/g, " ");
-  redSpan.innerHTML = value;
-  //   const arrayFromSplitted = value.split(",");
-  //   arrayFromSplitted.map();
+  const arrayFromSplitted = colorInput.value.split(",");
+  console.log(arrayFromSplitted);
+  const arrayReplaced = arrayFromSplitted.map((item) =>
+    item.replace(/\D/g, "")
+  );
+  console.log(arrayReplaced);
+
+  //   const value = colorInput.value.replace(/\D/g, "");
+  //   testSpan.innerHTML = value.trim();
+
   //   if (arrayFromSplitted.length === 3) {
-  //     alert("ok");
   //     const [r, g, b] = arrayFromSplitted;
   //     redSpan.innerHTML = r;
   //     greenSpan.innerHTML = g;
   //     blueSpan.innerHTML = b;
   //   } else {
-  //     redSpan.innerHTML = "not 3";
-  //     alert("ok");
+  //     alert(arrayFromSplitted.length);
   //   }
 });
