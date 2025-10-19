171c8784013937e5eae5e5804965bde3ba5476e1
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-10-19 11:07:42 +0200

    copy-to-clipboard of result rgb code

--- public/scripts/copy-to-clipboard.js
+++ public/scripts/copy-to-clipboard.js
@@ -1,43 +1,6 @@
-function setClipboard(elementId) {
-  const button = document.getElementById(elementId + "-button");
-  const element = document.getElementById(elementId);
-  const tooltip = new bootstrap.Tooltip(button, { trigger: "manual" });
-  button.onclick = function () {
-    navigator.clipboard.writeText(element.value).then(() => {
-      button.setAttribute("data-bs-original-title", "Zkopírováno do schránky.");
-      tooltip.show();
-      setTimeout(() => {
-        tooltip.hide();
-      }, 1500);
-    });
-  };
-}
+const rgbResultSpan = document.querySelector(".rgb-result");
+const copyButton = document.querySelector(".button-copy");
 
-const inputElements = [
-  "last-name",
-  "first-name",
-  "tel",
-  "birth-date",
-  "insurance-company",
-  "ssn",
-  "insurance-company-code",
-  "street",
-  "municipal-part",
-  "house-number",
-  "orientation-number",
-  "registration-number",
-  "city",
-  "postal-code",
-  "medicine-name",
-  "form",
-  "strength",
-  "packaging-size",
-  "administration",
-  // "atc-who",
-  "code-sukl",
-  "quantity",
-  "reimbursement",
-  "instructions",
-];
-
-inputElements.forEach(setClipboard);
+copyButton.onclick = function () {
+  navigator.clipboard.writeText(rgbResultSpan.innerHTML);
+};
