18ab8828a0fd32185d06969c72e3a446d6055fec
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-10-20 14:21:33 +0200

    color converter rgb-hex ok

--- public/scripts/color.js
+++ public/scripts/color.js
@@ -3,12 +3,17 @@ const rgbResult = document.querySelector(".rgb-result");
 const copyButtonRgb = document.querySelector(".button-copy-rgb");
 const hexResult = document.querySelector(".hex-result");
 const copyButtonHex = document.querySelector(".button-copy-hex");
+const hexResult2 = document.querySelector(".hex-result2");
+const copyButtonHex2 = document.querySelector(".button-copy-hex2");
 
 copyButtonRgb.onclick = function () {
   navigator.clipboard.writeText(rgbResult.innerHTML);
 };
 copyButtonHex.onclick = function () {
   navigator.clipboard.writeText(hexResult.innerHTML);
+};
+copyButtonHex2.onclick = function () {
+  navigator.clipboard.writeText(hexResult2.innerHTML);
 };
 
 colorInput.addEventListener("input", () => {
@@ -77,59 +82,19 @@ colorInput.addEventListener("input", () => {
     const hexString = `#${toHex(Number(r))}${toHex(Number(g))}${toHex(
       Number(b)
     )}`;
+    const hexString2 = hexString.substring(1);
     rgbResult.innerHTML = rgbString;
     hexResult.innerHTML = hexString;
+    hexResult2.innerHTML = hexString2;
     copyButtonRgb.style.display = "inline-block";
     copyButtonHex.style.display = "inline-block";
+    copyButtonHex2.style.display = "inline-block";
   } else {
     rgbResult.innerHTML = "";
     hexResult.innerHTML = "";
+    hexResult2.innerHTML = "";
     copyButtonRgb.style.display = "none";
     copyButtonHex.style.display = "none";
+    copyButtonHex2.style.display = "none";
   }
-
-  // -----------------------------------------------------------------------------------------------------------
-  // -----------------------------------------------------------------------------------------------------------
-  // make array of strings from value, which are separated by space
-  // const arraySplittedBySpace = value.split(" ");
-  if (arraySplittedByComma.length === 3) {
-    resultArray = arraySplittedByComma;
-  } else if (arraySplittedBySpace.length === 3) {
-    resultArray = arraySplittedBySpace;
-  }
-  let resultArrayClean = null;
-  if (resultArray) {
-    resultArray.forEach((element) => {
-      element = element.trim();
-    });
-    resultArrayClean = resultArray.map((item) => item.replace(/\D/g, ""));
-    let validColors =
-      resultArrayClean[0] &&
-      resultArrayClean[1] &&
-      resultArrayClean[2] &&
-      resultArrayClean[0] <= 255 &&
-      resultArrayClean[1] <= 255 &&
-      resultArrayClean[2] <= 255;
-    if (validColors) {
-      // alert("OK");
-    }
-  }
-  // console.log(resultArrayClean);
-
-  //   const arrayReplaced = arrayFromSplitted.map((item) =>
-  //     item.replace(/\D/g, "")
-  //   );
-  //   console.log(arrayReplaced);
-
-  //   const value = colorInput.value.replace(/\D/g, "");
-  //   testSpan.innerHTML = value.trim();
-
-  //   if (arrayFromSplitted.length === 3) {
-  //     const [r, g, b] = arrayFromSplitted;
-  //     redSpan.innerHTML = r;
-  //     greenSpan.innerHTML = g;
-  //     blueSpan.innerHTML = b;
-  //   } else {
-  //     alert(arrayFromSplitted.length);
-  //   }
 });
