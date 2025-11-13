80449c50e0629cb8283291c7d294a715e616adb1
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2025-11-13 10:26:18 +0100

    articles.js update

--- src/articles.js
+++ src/articles.js
@@ -1,24 +1,5 @@
 const articles = [
   {
-    id: 1,
-    headline: "Jak se zobrazují barvy na vašem webu",
-    url: "jak-se-zobrazuji-barvy-na-vasem-webu",
-    subheadline: `Na rozdíl od přírody nebo běžných věcí monitor, když ukazuje
-                grafiku na vašem webu (logo, fotky apod.), světlo vyzařuje a
-                nepohlcuje.`,
-    content: `Aby nějakou barvu vytvořil "namíchá" ji z červené, zelené a modré.
-              Nejmenší částečka, tzv. pixel, tak má 3 svítící body a každý z
-              nich umí svítit jednou z těchto barev různou intenzitou. <b
-                ><a href="/color" class="my-a">Zde</a></b
-              > si to sami vyzkoušejte a zadejte tři hodnoty (od 0 do 255)
-              oddělené čárkou nebo mezerou, postupně pro červenou, zelenou a
-              modrou. Když se tvoří internetová stránka, řeknete stejně
-              internetovému prohlížeči, že chcete například olivovou tímto
-              kódem: rgb(226, 226, 182).`,
-    date: "15.10.2025",
-    alt: "itesar | barvy na webu",
-  },
-  {
     id: 2,
     headline: "Nástroje pro vývoj webů",
     url: "nastroje-pro-vyvoj-webu",
@@ -41,7 +22,26 @@ const articles = [
               je naprostá svoboda - tedy co to má být, jak chci aby to vypadalo
               a co chci přesně aby to dělalo.`,
     date: "3.11.2025",
-    alt: "itesar | nástroje pro tvorbu webu",
+    alt: "itesar | Nástroje",
+  },
+  {
+    id: 1,
+    headline: "Jak se zobrazují barvy na monitoru?",
+    url: "jak-se-zobrazuji-barvy-na-vasem-monitoru?",
+    subheadline: `Na rozdíl od přírody nebo běžných věcí monitor, když ukazuje
+                grafiku na vašem webu (logo, fotky apod.), světlo vyzařuje a
+                nepohlcuje.`,
+    content: `Aby nějakou barvu vytvořil "namíchá" ji z červené, zelené a modré.
+              Nejmenší částečka, tzv. pixel, tak má 3 svítící body a každý z
+              nich umí svítit jednou z těchto barev různou intenzitou. <b
+                ><a href="/color" class="my-a">Zde</a></b
+              > si to sami vyzkoušejte a zadejte tři hodnoty (od 0 do 255)
+              oddělené čárkou nebo mezerou, postupně pro červenou, zelenou a
+              modrou. Když se tvoří internetová stránka, řeknete stejně
+              internetovému prohlížeči, že chcete například olivovou tímto
+              kódem: rgb(226, 226, 182).`,
+    date: "15.10.2025",
+    alt: "itesar | Obraz s barvami",
   },
 ];
 
