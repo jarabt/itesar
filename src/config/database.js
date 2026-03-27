6a58f0a64522c5fe6fef8bcb372eb132bf1d5973
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2026-03-27 11:03:38 +0100

    connected pg pool

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import pg from "pg";
+import config from "./environment.js";
+
+// connect-pg-simple requires a Pool or conString, not a single Client
+const pool = new pg.Pool(config.database);
+
+pool.on("connect", () => {
+  // console.log("PostgreSQL pool connected");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG pool error", err);
+  process.exit(1);
+});
+
+export { pool };
--- src/config/environment.js
+++ src/config/environment.js
@@ -6,6 +6,13 @@ const config = {
   app: {
     port: process.env.APP_PORT,
   },
+  database: {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  },
 };
 
 export default config;
