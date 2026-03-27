3960a47d436d36bcaa7e586e622d6fc17416afcb
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2026-03-27 11:49:10 +0100

    supplier in env

--- src/config/environment.js
+++ src/config/environment.js
@@ -13,6 +13,20 @@ const config = {
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
   },
+  supplier: {
+    title: process.env.SUPPLIER_TITLE,
+    subtitle: process.env.SUPPLIER_SUBTITLE,
+    name: process.env.SUPPLIER_NAME,
+    ico: process.env.SUPPLIER_ICO,
+    address: process.env.SUPPLIER_ADDRESS,
+    postalCode: process.env.SUPPLIER_POSTAL_CODE,
+    city: process.env.SUPPLIER_CITY,
+    email: process.env.SUPPLIER_EMAIL,
+    phone: process.env.SUPPLIER_PHONE,
+    bankAccount: process.env.SUPPLIER_BANK_ACCOUNT,
+    paymentMethod: process.env.SUPPLIER_PAYMENT_METHOD,
+    currency: process.env.SUPPLIER_CURRENCY,
+  },
 };
 
 export default config;
