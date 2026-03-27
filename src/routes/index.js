1b2bde65632f17f46bdce9472690f5f55d86eeb3
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2026-03-27 11:59:52 +0100

    npm i pdfkit, add routes to Router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import homeRoutes from "./homeRoutes.js";
+import customerRoutes from "./customerRoutes.js";
+import invoiceRoutes from "./invoiceRoutes.js";
 
 const router = Router();
 
 router.use(homeRoutes);
+router.use(customerRoutes);
+router.use(invoiceRoutes);
 
 export default router;
